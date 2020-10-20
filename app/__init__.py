d54d2b28ba7e5fff5652de67d747baaa39f63e8a
kiptoo-097 <kiptoo097.com>
2020-10-20 19:49:55 +0300

    added confiqurations

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+  app = Flask(_name_)
+
+  #create app configuraions
+  app.config.from_object(config_options[config_name])
+
+  bootstrap.init_app(app)
+
+    # Registering the blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+    # setting config
+  from .requests import configure_request
+  configure_request(app)
+
+
+  return app
---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}

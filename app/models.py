90f064d8e980992ea82e51166bf150b1a433c2f4
kiptoo-097 <kiptoo097.com>
2020-10-20 19:48:56 +0300

    added classes

---
+++ app/models.py
@@ -0,0 +1,31 @@
+class Source:
+    
+    """
+    class to define the source of news
+    """
+
+    def _init_(self,id,name,description,url,category,language,country):
+
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
+class Article:
+
+    """
+    class to define the news article
+    """
+
+    def _init_(self,id,author,title,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content

309b60941823ecf1ff334d80f50f4da25407f59b
kiptoo-097 <kiptoo097.com>
2020-10-22 02:43:30 +0300

    modified apikey variable

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 class Config:
+
     NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
+    
 
 class ProdConfig(Config):
     pass
